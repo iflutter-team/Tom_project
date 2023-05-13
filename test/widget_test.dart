4ca7aa05665655b5c95bac41b1182b05ffed6e18
Vandan Taraviya <121664941+vandantaraviya@users.noreply.github.com>
2023-05-13 10:30:36 +0530

    delete package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tom_project/main.dart';
 import 'package:tom_project/screen/splash%20screen/Splash_screen.dart';
 
 void main() {
